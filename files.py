2a09b91b9e447c718b37d7c472e310eb28bc5cd1
Aditya Chate <aditya.chate297@gmail.com>
2023-02-24 15:07:17 +0530

    json/pickling implemented

---
+++ files.py
@@ -0,0 +1,17 @@
+with open('demo.txt', mode='w') as f:
+    # f.write("Add content to Python Write.\n")
+    # file_content = f.readlines()
+    # f.close()
+
+    # for line in file_content:
+    #     print(line[:-1])
+    # line = f.readline()
+
+    # while line:
+    #     print(line)
+    #     line = f.readline()
+    f.write("Testing file close.")
+
+# f.close()
+user_input = input("Testing: ")
+print("Done.")
